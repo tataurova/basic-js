e7a8a712b33536cc5cb15d72d7b425581388f4fb
tataurova <tataurovaoe@yandex.ru>
2020-09-23 01:55:55 +0300

    refactor: refactor code for VigenereCipheringMachine class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,5 @@
-const CustomError = require("../extensions/custom-error");
+const ALPHABET_LENGTH = 26;
+const ASCII_DELTA = 96;
 
 class VigenereCipheringMachine {
   constructor(isDirectMode = true) {
@@ -6,59 +7,68 @@ class VigenereCipheringMachine {
   }
 
   encrypt(phrase, key) {
-    let phraseArray = phrase.split(``);
-    let keyArray = key.split(``);
-    let fullKeyArray = []; // forming key through all string
+    phrase = phrase.toLowerCase();
+    this._phraseArray = phrase.split(``);
 
-    let counter = keyArray.length;
-    let j = 0;
-
-    for (let i = 0; i < phraseArray.length; i++) {
-
-      if (/[a-zA-Z]/.test(phraseArray[i])) {
-        fullKeyArray.push(keyArray[j]);
-        j++;
-        if (j >= counter) {
-          j = 0;
-        }
-      } else {
-        fullKeyArray.push(phraseArray[i]);
-      }
-
-    }
+    const fullKeyArray = this._formKeyArray(this._phraseArray, key);
 
     let resultNumberArray = []; // forming numbers of target letters
     for (let i = 0; i < phrase.length; i++) {
       let resultNumber;
-      if (/[a-zA-Z]/.test(phraseArray[i])) {
-        if (phrase[i].charCodeAt() - 96 === fullKeyArray[i].charCodeAt() - 96) {
-          resultNumber = phrase[i].charCodeAt() - 96
-        } else if (phrase[i].charCodeAt() - 96 + fullKeyArray[i].charCodeAt() - 96 > 26) {
-          resultNumber = phrase[i].charCodeAt() - 96 + fullKeyArray[i].charCodeAt() - 96 - 26 - 1;
+      const letterNumber = phrase[i].charCodeAt() - ASCII_DELTA;
+      const keyNumber = fullKeyArray[i].charCodeAt() - ASCII_DELTA;
+
+      if (/[a-zA-Z]/.test(this._phraseArray[i])) { // add letter number to result array
+        if (letterNumber + keyNumber > ALPHABET_LENGTH) {
+          resultNumber = (letterNumber + keyNumber) - ALPHABET_LENGTH - 1;
         } else {
-          resultNumber = phrase[i].charCodeAt() - 96 + fullKeyArray[i].charCodeAt() - 96 - 1;
+          resultNumber = letterNumber + keyNumber - 1;
         }
         resultNumberArray.push(resultNumber);
       } else {
-        resultNumberArray.push(phraseArray[i]);
+        resultNumberArray.push(this._phraseArray[i]); // add symbol to result array
       }
 
     }
-    let resultArray = [];
-    for (let i = 0; i < resultNumberArray.length; i++) { // forming target string
-      if (typeof resultNumberArray[i] === `number`) {
-        resultArray.push(String.fromCharCode(96 + resultNumberArray[i]));
-      } else {
-        resultArray.push(resultNumberArray[i]);
-      }
-    }
+
+    const resultArray = this._getResultString(resultNumberArray);
     return this.directMode ? resultArray.join(``).toUpperCase() : resultArray.reverse().join(``).toUpperCase();
   }
 
   decrypt(phrase, key) {
     phrase = phrase.toLowerCase();
-    let phraseArray = phrase.split(``);
-    let keyArray = key.split(``);
+    this._phraseArray = phrase.split(``);
+
+    const fullKeyArray = this._formKeyArray(phrase, key);
+
+    let resultNumberArray = []; // forming numbers of target letters
+    for (let i = 0; i < phrase.length; i++) {
+      let resultNumber;
+      const letterNumber = phrase[i].charCodeAt() - ASCII_DELTA;
+      const keyNumber = fullKeyArray[i].charCodeAt() - ASCII_DELTA;
+
+      if (/[a-zA-Z\`]/.test(this._phraseArray[i])) { // add letter number to result array
+
+        if (letterNumber === keyNumber) {
+          resultNumber = 1;
+
+        } else if (letterNumber - keyNumber < 0) {
+          resultNumber = ALPHABET_LENGTH - (keyNumber - letterNumber) + 1;
+
+        } else {
+          resultNumber = letterNumber - keyNumber + 1;
+        }
+        resultNumberArray.push(resultNumber);
+      } else {
+        resultNumberArray.push(this._phraseArray[i]); // add symbol to result array
+      }
+    }
+    const resultArray = this._getResultString(resultNumberArray);
+    return this.directMode ? resultArray.join(``).toUpperCase() : resultArray.reverse().join(``).toUpperCase();
+  }
+
+  _formKeyArray(phraseArray, key) {
+    let keyArray = key.toLowerCase().split(``);
     let fullKeyArray = []; // forming key through all string
 
     let counter = keyArray.length;
@@ -66,7 +76,7 @@ class VigenereCipheringMachine {
 
     for (let i = 0; i < phraseArray.length; i++) {
 
-      if (/[a-zA-Z]/.test(phraseArray[i])) {
+      if (/[a-zA-Z`]/.test(phraseArray[i])) {
         fullKeyArray.push(keyArray[j]);
         j++;
         if (j >= counter) {
@@ -76,26 +86,10 @@ class VigenereCipheringMachine {
         fullKeyArray.push(phraseArray[i]);
       }
     }
+    return fullKeyArray;
+  }
 
-    let resultNumberArray = []; // forming numbers of target letters
-    for (let i = 0; i < phrase.length; i++) {
-      let resultNumber;
-      if (/[a-zA-Z]/.test(phraseArray[i])) {
-
-        if (phrase[i].charCodeAt() - 96 === fullKeyArray[i].charCodeAt() - 96) {
-          resultNumber = 1;
-
-        } else if ((phrase[i].charCodeAt() - 96) - (fullKeyArray[i].charCodeAt() - 96) < 0) {
-          resultNumber = 26 - ((fullKeyArray[i].charCodeAt() - 96) - (phrase[i].charCodeAt() - 96)) + 1;
-
-        } else {
-          resultNumber = ((phrase[i].charCodeAt() - 96) - (fullKeyArray[i].charCodeAt() - 96)) +1;
-        }
-        resultNumberArray.push(resultNumber);
-      } else {
-        resultNumberArray.push(phraseArray[i]) +1;
-      }
-    }
+  _getResultString(resultNumberArray) {
     let resultArray = [];
     for (let i = 0; i < resultNumberArray.length; i++) { // forming target string
       if (typeof resultNumberArray[i] === `number`) {
@@ -104,7 +98,7 @@ class VigenereCipheringMachine {
         resultArray.push(resultNumberArray[i]);
       }
     }
-    return this.directMode ? resultArray.join(``).toUpperCase() : resultArray.reverse().join(``).toUpperCase();
+    return resultArray;
   }
 }
 
